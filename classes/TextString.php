f94d6f95f374396b977688c8bc354ad6aeae88f9
Jared <Jared@PCI>
2024-08-28 22:52:12 -0400

    rebuild

--- classes/TextString.php
+++ classes/TextString.php
@@ -19,10 +19,89 @@ class TextString
     
     // escape sql characters to prevent injection
     public static function db_sanitize($string)
-    {
-        $chars = array("\\","'","`");
-        $replace = array("\\\\","\\'","\\`");
-        return str_replace($chars,$replace,$string);
+    {   
+        $translate = array(
+            "\xC2\x82" => "'", // U+0082⇒U+201A single low-9 quotation mark
+            chr(0xc2).chr(0x82) => "'",
+            
+           "\xC2\x84" => '"', // U+0084⇒U+201E double low-9 quotation mark
+            chr(0xc2).chr(0x84) => '"',
+            
+           "\xC2\x8B" => "'", // U+008B⇒U+2039 single left-pointing angle quotation mark
+            chr(0xc2).chr(0x8b) => "'",
+            
+           "\xC2\x91" => "'", // U+0091⇒U+2018 left single quotation mark
+            chr(0xc2).chr(0x91) => "'",
+            
+           "\xC2\x92" => "'", // U+0092⇒U+2019 right single quotation mark
+            chr(0xc2).chr(0x92) => "'",
+            
+           "\xC2\x93" => '"', // U+0093⇒U+201C left double quotation mark
+            chr(0xc2).chr(0x93) => '"',
+            
+           "\xC2\x94" => '"', // U+0094⇒U+201D right double quotation mark
+            chr(0xc2).chr(0x94) => '"',
+            
+           "\xC2\x9B" => "'", // U+009B⇒U+203A single right-pointing angle quotation mark
+            chr(0xc2).chr(0x9b) => "'",
+            
+           "\xC2\xAB" => '"', // U+00AB left-pointing double angle quotation mark
+            chr(0xc2).chr(0xab) => '"',
+            
+           "\xC2\xBB" => '"', // U+00BB right-pointing double angle quotation mark
+            chr(0xc2).chr(0xbb) => '"',
+            
+           "\xE2\x80\x98" => "'", // U+2018 left single quotation mark
+            chr(0xe2).chr(0x80).chr(0x98) => "'",
+            
+           "\xE2\x80\x99" => "'", // U+2019 right single quotation mark
+            chr(0xe2).chr(0x80).chr(0x99) => "'",
+            
+           "\xE2\x80\x9A" => "'", // U+201A single low-9 quotation mark
+            chr(0xe2).chr(0x80).chr(0x9a) => "'",
+            
+           "\xE2\x80\x9B" => "'", // U+201B single high-reversed-9 quotation mark
+            chr(0xe2).chr(0x80).chr(0x9b) => "'",
+            
+           "\xE2\x80\x9C" => '"', // U+201C left double quotation mark
+            chr(0xe2).chr(0x80).chr(0x9c) => '"',
+            
+           "\xE2\x80\x9D" => '"', // U+201D right double quotation mark
+            chr(0xe2).chr(0x80).chr(0x9d) => '"',
+            
+           "\xE2\x80\x9E" => '"', // U+201E double low-9 quotation mark
+            chr(0xe2).chr(0x80).chr(0x9e) => '"',
+            
+           "\xE2\x80\x9F" => '"', // U+201F double high-reversed-9 quotation mark
+            chr(0xe2).chr(0x80).chr(0x9f) => '"',
+            
+           "\xE2\x80\xB9" => "'", // U+2039 single left-pointing angle quotation mark
+            chr(0xe2).chr(0x80).chr(0xb9) => "'",
+            
+           "\xE2\x80\xBA" => "'", // U+203A single right-pointing angle quotation mark
+            chr(0xe2).chr(0x80).chr(0xba) => "'",
+            
+            chr(0xe2).chr(0x80).chr(0x9a) => '\'', //SINGLE LOW-9 QUOTATION MARK
+            chr(0xe2).chr(0x80).chr(0x9e) => '"', //DOUBLE LOW-9 QUOTATION MARK
+            chr(0xe2).chr(0x80).chr(0xa6) => '...', //HORIZONTAL ELLIPSIS
+            chr(0xe2).chr(0x80).chr(0x98) => '\'', //LEFT SINGLE QUOTATION MARK
+            chr(0xe2).chr(0x80).chr(0x99) => '\'', //RIGHT SINGLE QUOTATION MARK
+            chr(0xe2).chr(0x80).chr(0x9c) => '"', //LEFT DOUBLE QUOTATION MARK
+            chr(0xe2).chr(0x80).chr(0x9d) => '"', //RIGHT DOUBLE QUOTATION MARK
+            chr(0xe2).chr(0x80).chr(0x93) => '-', //EN DASH
+            chr(0xe2).chr(0x80).chr(0x94) => '-', //EM DASH
+            chr(145) => "'",
+            chr(146) => "'",
+            chr(147) => '"',
+            chr(148) => '"',
+            chr(151) => '-',
+            "\\" => "\\\\",
+            "'" => "\\'",
+            "`" => "\\`",
+           
+        );
+        
+        return  mb_convert_encoding(iconv('UTF-8', 'ASCII//TRANSLIT', str_replace(array_keys($translate), $translate, $string)), 'HTML-ENTITIES', 'UTF-8');
     }
     
     // strip all non alpha-numeric
